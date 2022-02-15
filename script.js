0abf82f7ae70982f637fc59f41d54334f6e7042a
aditi-hinge <aditi.hinge@gmail.com>
2022-02-15 19:30:51 +0900

    index.html

--- script.js
+++ script.js
@@ -3,7 +3,44 @@ let screen = document.getElementById("screen");
 let equalsButton = document.getElementById("equals-button");
 let acButton = document.getElementById("ac-button");
 let brackets = document.getElementById("brackets");
+let solutionDisplay = document.getElementById("solution-display");
+let equationDisplay = document.getElementById("equation-display");
 
+//subtract
+let subtract = (num1, num2) => {
+    solutionDisplay.textContent = +num1 - +num2;
+}
+
+//multiply
+let multiply = (num1, num2) => {
+    // solutionDisplay.textContent ="";
+    solutionDisplay.textContent = +num1 * +num2;
+}
+
+//divide
+let divide = (num1, num2) => {
+    if (+num2 === 0) {solutionDisplay.textContent = "Can't divide by 0";}
+    else {solutionDisplay.textContent = +num1 / +num2;}
+    }
+
+//add
+let add = (num1,num2) => {
+    solutionDisplay.textContent = +num1 + +num2;
+  };
+
+let operate = (num1,operator,num2) => {
+    if(operator === " + "){
+        add(num1,num2);
+    } else if (operator === " - "){
+        subtract(num1,num2);
+    } else if (operator === " * "){
+        multiply(num1,num2);
+    } else if (operator === " / "){
+        divide(num1,num2);
+    } else {
+        return "error";
+    }
+ };
 //click animation
 document.querySelectorAll(".buttons").forEach(button => {
     button.addEventListener("click",() =>{
@@ -12,61 +49,56 @@ document.querySelectorAll(".buttons").forEach(button => {
     })
 });
 
+
+
+//function for displaying and storing values
+
 //display buttons on screen
-document.querySelectorAll(".buttons").forEach(button => {
-    button.addEventListener("click", () => {
-        screen.textContent += button.textContent;
-    })
+document.querySelectorAll(".numbers").forEach(number => {
+    let store = () => {
+        solutionDisplay.textContent += number.textContent;
+    }
+    number.addEventListener("click",store);
 });
+
+document.querySelectorAll(".operators").forEach(operator => {
+    operator.addEventListener("click", () => {
+        let num1 = solutionDisplay.textContent;
+        let operatorChoice = operator.textContent;
+        solutionDisplay.textContent += operatorChoice;
+
+            equalsButton.addEventListener("click", () => {
+                let arrayFromDisplay = solutionDisplay.textContent.split(" ");
+                let num2 = arrayFromDisplay[arrayFromDisplay.length - 1];
+                finalDisplay = solutionDisplay.textContent;
+                equationDisplay.textContent = finalDisplay;
+                operate(num1,operatorChoice,num2);
+                });
+      })
+   })
+
+
+
 
 //AC button
 acButton.addEventListener("click", () => {
-    screen.textContent = "";
+    finalDisplay = "";
+    solutionDisplay.textContent = "";
+    equationDisplay.textContent = "";
 })
 
-//
-brackets.addEventListener("click", () => {
-    if(!")") {
-
-    }
-})
-
-
-//subtract
-let subtract = (num1, num2) => {
-    screen.textContent = num1 - num2;
-}
-
-//multiply
-let multiply = (num1, num2) => {
-    screen.textContent = num1 * num2;
-}
-
-//divide
-let divide = (num1, num2) => {
-    screen.textContent = num1 / num2;
-}
-
-//add
-let add = (num1,num2) => {
-    screen.textContent = +num1 + +num2;
-  };
-let operate = (num1,operator,num2) => {
-    if(operator === "+"){
-        add(num1,num2);
-    } else if (operator === "-"){
-        subtract(num1,num2);
-    } else if (operator === "*"){
-        multiply(num1,num2);
-    } else if (operator === "/"){
-        divide(num1,num2);
-    } else {
-        return "error";
-    }
- };
-// operate(3,"/",4);
-
-equalsButton.addEventListener("click", () => {
+//delete button SOLVED
+const deleteButton = document.getElementById("delete");
+deleteButton.addEventListener("click", ()=>{
+    if(!solutionDisplay.textContent){equationDisplay.textContent = "";
+    console.log(solutionDisplay.textContent);
+    return;}
+    let display = solutionDisplay.textContent;
+        let lastNumberArray = display.split("");
+        lastNumberArray.pop().toString();
+    solutionDisplay.textContent = lastNumberArray.join("");
     
-    operate(num1,num2);
-});
+    });
+
+    //arrayFromDisplay finalDisplay num2 num1 operatorChoice lastNumberArray solutionDisplay.textContent
+    
